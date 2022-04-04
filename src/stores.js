5b36a4f61b3cab1cb0d248a2229a8c67ea12b6cd
Your Name <pcismyname007@gmail.com>
2022-04-05 00:13:57 +0700

    add id

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,8 @@
 import {writable} from 'svelte/store';
 
 let data = {'6410742040':{number: '6410742040', pin: '1111'},
-            '2222':{number: '2222', pin: '2222'}};
+            '2222':{number: '2222', pin: '2222'},
+            '1111':{number: '1111', pin: '1111'}};
 
 export const accounts = writable(data);
 export const islogin = writable(false);
